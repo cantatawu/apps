2aaf7118e3fb6f81b4f428ecf73ea5dda6871942
wuzhengquan <wuzhengquan@zhixueyun.com>
2017-11-10 15:44:37 +0800

    add

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app" class="app-index-page">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome'
+    }
+  }
+}
+</script>
+
